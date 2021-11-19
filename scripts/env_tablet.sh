4cd0cc6eddd0ac2aa19f4670459e8d35b03e2774
Burak Aksoy <burakaksoy20@gmail.com>
2021-11-19 10:26:44 -0500

    Temporary IP change for master 100 to 200

--- scripts/env_tablet.sh
+++ scripts/env_tablet.sh
@@ -6,7 +6,7 @@ source $ROS_NOETIC/setup.bash
 source $ROS_WS/devel/setup.bash
 export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
-export ROS_MASTER_URI=http://192.168.1.100:11311/
+export ROS_MASTER_URI=http://192.168.1.200:11311/
 export ROS_IP=192.168.1.99
 echo 1234 | sudo -S chmod 666 /dev/ttyACM*
 
