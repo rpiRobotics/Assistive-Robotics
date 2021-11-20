53be2571dc4ddbf89650c79c94f170d2a1c82db3
Burak Aksoy <burakaksoy20@gmail.com>
2021-11-20 15:10:38 -0500

    Add export display to oarbot env files for kinect

--- scripts/env_oarbot_silver.sh
+++ scripts/env_oarbot_silver.sh
@@ -8,5 +8,6 @@ export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_MASTER_URI=http://192.168.1.200:11311/
 export ROS_IP=192.168.1.101
+export DISPLAY:=1
 
 exec "$@"
