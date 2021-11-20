457418dc93f87da0f0b1720ccdcd99db96cb1609
Burak Aksoy <burakaksoy20@gmail.com>
2021-11-20 15:16:42 -0500

    try display 0

--- scripts/env_oarbot_blue.sh
+++ scripts/env_oarbot_blue.sh
@@ -8,6 +8,6 @@ export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_MASTER_URI=http://192.168.1.200:11311/
 export ROS_IP=192.168.1.102
-export DISPLAY:=1
+export DISPLAY:=0
 
 exec "$@"
