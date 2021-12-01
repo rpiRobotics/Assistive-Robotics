042774bb109a62ce7ad821ea4405700f61891db7
Burak Aksoy <burakaksoy20@gmail.com>
2021-12-01 08:58:10 -0500

    try to launch joint followers inside the robots

---
+++ test_launcher-body_joint_follower.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 1s;
+gnome-terminal --tab --title="JOINT_FOLLOWER_BLUE" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch  oarbot_blue_body_single_joint_follower.launch; exec bash\"";
+gnome-terminal --tab --title="JOINT_FOLLOWER_SILVER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch  oarbot_silver_body_single_joint_follower.launch; exec bash\"";
+
+
