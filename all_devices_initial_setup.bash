2e7c73bf71a84997fc4513a0041df2170f5d52b6
Burak Aksoy <burakaksoy20@gmail.com>
2021-12-30 14:40:29 -0500

    Works successfuly: All Devices Initial Setup New bash

--- all_devices_initial_setup.bash
+++ all_devices_initial_setup.bash
@@ -4,6 +4,7 @@ USERNAMES=("tablet" "razer" "oarbot_silver" "oarbot_blue" )
 PASSWORDS=("1234" "1234" "1234" "1234" )
 
 SCRIPTS=(
+    ############### TABLET ##############
     "cd; 
     mkdir catkin_ws_assistive; 
     cd catkin_ws_assistive; 
@@ -21,39 +22,68 @@ SCRIPTS=(
     pip3 install pyserial
     "
 
-
-
-
-
-
-
-
-    "cd; 
+    ############### RAZER (MAIN COMPUTER) ##############
+    "cd;
+    echo 1234 | sudo -S usermod -a -G dialout razer;
+    echo 1234 | sudo -S apt install -y curl;
+    echo 1234 | sudo -S apt install -y software-properties-common;
+    echo 1234 | sudo -S apt install -y spacenavd;
+    echo 1234 | sudo -S apt install -y ros-melodic-spacenav-node;
+    echo 1234 | sudo -S apt install -y ros-melodic-twist-mux;
+    echo 1234 | sudo -S apt install -y ros-melodic-moveit*;
+    echo 1234 | sudo -S apt install -y python3-pip;
+    echo 1234 | sudo -S apt install -y python-pip;
+    pip3 install pyserial;
+    pip install general-robotics-toolbox;
+    if ! grep -q '^deb .*https://packages.microsoft.com/ubuntu/18.04/prod' /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+        echo 1234 | curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -;
+        echo 1234 | sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/prod;
+        echo 1234 | sudo apt-get update;
+    fi;
+    export DEBIAN_FRONTEND=noninteractive;
+    echo 1234 | echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4abt1.1 libk4abt1.1/accept-eula boolean true' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4abt1.1 libk4abt1.1/accepted-eula-hash string 03a13b63730639eeb6626d24fd45cf25131ee8e8e0df3f1b63f552269b176e38' | sudo debconf-set-selections;
+    echo 1234 | sudo apt install -y libk4a1.4-dev;
+    # sudo apt-get --purge --reinstall install libk4a1.4-dev # If something goes wrong
+    echo 1234 | sudo apt install -y libk4abt1.1-dev;
+    # sudo apt-get --purge --reinstall install libk4abt1.1-dev # If something goes wrong
+    echo 1234 | sudo apt install -y k4a-tools;
+    # sudo apt-get --purge --reinstall install k4a-tools # If something goes wrong
+    echo 1234 | sudo apt install -y ros-melodic-rgbd-launch;
+    cd /etc/udev/rules.d;
+    echo 1234 | sudo wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules;
+    echo 1234 | sudo wget https://raw.githubusercontent.com/Kinovarobotics/kinova-ros/melodic-devel/kinova_driver/udev/10-kinova-arm.rules;
+    echo 1234 | sudo rm 10-kinova-arm.rules.*; # removes the duplicates
+    echo 1234 | sudo rm 99-k4a.rules.*; # removes the duplicates
+    ##################
+    cd; 
     mkdir catkin_ws_assistive; 
     cd catkin_ws_assistive; 
     rm -rf {*,.*};
     git clone https://github.com/rpiRobotics/Assistive-Robotics.git .;
+    cd src;
+    git clone https://github.com/burakaksoy/Azure_Kinect_ROS_Driver.git;
+    git clone https://github.com/burakaksoy/kinova-ros.git kinova-ros;
+
+    cd ..;
+    ##############
     source /opt/ros/melodic/setup.bash;
-    catkin_make -DCATKIN_WHITELIST_PACKAGES='assistive_msgs;assistive_gui;assistive_launch';
+    catkin_make -DCATKIN_BLACKLIST_PACKAGES='arduino_talker';
+    # catkin_make -DCATKIN_WHITELIST_PACKAGES='assistive_msgs;assistive_gui;assistive_launch';
     grep -qxF 'source ~/catkin_ws_assistive/devel/setup.bash' ~/.bashrc || echo 'source ~/catkin_ws_assistive/devel/setup.bash' >> ~/.bashrc;
     grep -qxF 'export ROS_IP=192.168.1.100' ~/.bashrc || echo 'export ROS_IP=192.168.1.100' >> ~/.bashrc;
     grep -qxF 'export ROSLAUNCH_SSH_UNKNOWN=1' ~/.bashrc || echo 'export ROSLAUNCH_SSH_UNKNOWN=1' >> ~/.bashrc;
     source ~/.bashrc;
-    source ~/catkin_ws_assistive/devel/setup.bash;
-    echo 1234 | sudo -S apt install -y spacenavd;
-    echo 1234 | sudo -S apt install -y ros-melodic-spacenav-node;
-    echo 1234 | sudo -S apt install -y ros-melodic-twist-mux;
-    echo 1234 | sudo -S usermod -a -G dialout razer
+    source ~/catkin_ws_assistive/devel/setup.bash;    
     "
 
-
-
-
-
-
-
+    ############### OARBOT_SILVER ##############
     "cd;
     echo 1234 | sudo -S usermod -a -G dialout oarbot_silver;
+    echo 1234 | sudo -S apt install -y curl;
+    echo 1234 | sudo -S apt install -y software-properties-common;
     echo 1234 | sudo -S apt install -y ros-melodic-twist-mux;
     echo 1234 | sudo -S apt install -y ros-melodic-moveit*;
     echo 1234 | sudo -S apt install -y python3-pip;
@@ -64,7 +94,12 @@ SCRIPTS=(
         echo 1234 | curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -;
         echo 1234 | sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/prod;
         echo 1234 | sudo apt-get update;
-    fi
+    fi;
+    export DEBIAN_FRONTEND=noninteractive;
+    echo 1234 | echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4abt1.1 libk4abt1.1/accept-eula boolean true' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4abt1.1 libk4abt1.1/accepted-eula-hash string 03a13b63730639eeb6626d24fd45cf25131ee8e8e0df3f1b63f552269b176e38' | sudo debconf-set-selections;
     echo 1234 | sudo apt install -y libk4a1.4-dev;
     # sudo apt-get --purge --reinstall install libk4a1.4-dev # If something goes wrong
     echo 1234 | sudo apt install -y libk4abt1.1-dev;
@@ -75,8 +110,8 @@ SCRIPTS=(
     cd /etc/udev/rules.d;
     echo 1234 | sudo wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules;
     echo 1234 | sudo wget https://raw.githubusercontent.com/Kinovarobotics/kinova-ros/melodic-devel/kinova_driver/udev/10-kinova-arm.rules;
-    echo 1234 | sudo rm 10-kinova-arm.rules.* # removes the duplicates
-    echo 1234 | sudo rm 99-k4a.rules.* # removes the duplicates
+    echo 1234 | sudo rm 10-kinova-arm.rules.*; # removes the duplicates
+    echo 1234 | sudo rm 99-k4a.rules.*; # removes the duplicates
     ##################
     cd; 
     mkdir catkin_ws_assistive; 
@@ -90,23 +125,17 @@ SCRIPTS=(
     cd ..;
     ##############
     source /opt/ros/melodic/setup.bash;
-    catkin_make -DCATKIN_BLACKLIST_PACKAGES='assistive_gui;assistive_launch;arduino_talker';
+    catkin_make -DCATKIN_BLACKLIST_PACKAGES='assistive_gui;arduino_talker';
     grep -qxF 'source ~/catkin_ws_assistive/devel/setup.bash' ~/.bashrc || echo 'source ~/catkin_ws_assistive/devel/setup.bash' >> ~/.bashrc;
     source ~/.bashrc;
-    source ~/catkin_ws_assistive/devel/setup.bash    
+    source ~/catkin_ws_assistive/devel/setup.bash;    
     "
-    
 
-
-
-
-
-
-
-
-
+    ############### OARBOT_BLUE ##############
     "cd;
     echo 1234 | sudo -S usermod -a -G dialout oarbot_blue;
+    echo 1234 | sudo -S apt install -y curl;
+    echo 1234 | sudo -S apt install -y software-properties-common;
     echo 1234 | sudo -S apt install -y ros-melodic-twist-mux;
     echo 1234 | sudo -S apt install -y ros-melodic-moveit*;
     echo 1234 | sudo -S apt install -y python3-pip;
@@ -117,7 +146,12 @@ SCRIPTS=(
         echo 1234 | curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -;
         echo 1234 | sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/prod;
         echo 1234 | sudo apt-get update;
-    fi
+    fi;
+    export DEBIAN_FRONTEND=noninteractive;
+    echo 1234 | echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4abt1.1 libk4abt1.1/accept-eula boolean true' | sudo debconf-set-selections;
+    echo 1234 | echo 'libk4abt1.1 libk4abt1.1/accepted-eula-hash string 03a13b63730639eeb6626d24fd45cf25131ee8e8e0df3f1b63f552269b176e38' | sudo debconf-set-selections;
     echo 1234 | sudo apt install -y libk4a1.4-dev;
     # sudo apt-get --purge --reinstall install libk4a1.4-dev # If something goes wrong
     echo 1234 | sudo apt install -y libk4abt1.1-dev;
@@ -128,8 +162,8 @@ SCRIPTS=(
     cd /etc/udev/rules.d;
     echo 1234 | sudo wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules;
     echo 1234 | sudo wget https://raw.githubusercontent.com/Kinovarobotics/kinova-ros/melodic-devel/kinova_driver/udev/10-kinova-arm.rules;
-    echo 1234 | sudo rm 10-kinova-arm.rules.* # removes the duplicates
-    echo 1234 | sudo rm 99-k4a.rules.* # removes the duplicates
+    echo 1234 | sudo rm 10-kinova-arm.rules.*; # removes the duplicates
+    echo 1234 | sudo rm 99-k4a.rules.*; # removes the duplicates
     ##################
     cd; 
     mkdir catkin_ws_assistive; 
@@ -143,10 +177,10 @@ SCRIPTS=(
     cd ..;
     ##############
     source /opt/ros/melodic/setup.bash;
-    catkin_make -DCATKIN_BLACKLIST_PACKAGES='assistive_gui;assistive_launch;arduino_talker';
+    catkin_make -DCATKIN_BLACKLIST_PACKAGES='assistive_gui;arduino_talker';
     grep -qxF 'source ~/catkin_ws_assistive/devel/setup.bash' ~/.bashrc || echo 'source ~/catkin_ws_assistive/devel/setup.bash' >> ~/.bashrc;
     source ~/.bashrc;
-    source ~/catkin_ws_assistive/devel/setup.bash    
+    source ~/catkin_ws_assistive/devel/setup.bash;    
     ")
 
 echo ${SCRIPTS}
