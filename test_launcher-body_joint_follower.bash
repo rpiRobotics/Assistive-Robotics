943773bc95f2d4a8dc36e3e506ea70af7103f935
Burak Aksoy <burakaksoy20@gmail.com>
2022-02-08 16:05:55 -0500

    debug

--- test_launcher-body_joint_follower.bash
+++ test_launcher-body_joint_follower.bash
@@ -4,5 +4,5 @@ gnome-terminal --tab --title="JOINT_FOLLOWER_BLUE" --command "bash -c \"source ~
 gnome-terminal --tab --title="JOINT_FOLLOWER_SILVER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch  oarbot_silver_body_single_joint_follower.launch; exec bash\"";
 
 # Also start recording from overhead camera
-# gnome-terminal --tab --title="NUC RECORDER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch rosbag_record_nuc.launch; exec bash\"";
+gnome-terminal --tab --title="NUC RECORDER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch rosbag_record_nuc.launch; exec bash\"";
 
