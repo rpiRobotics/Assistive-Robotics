def33ef3bef2f33f76f1fdd59cbd255c1f844fe8
Burak Aksoy <burakaksoy20@gmail.com>
2022-06-02 16:06:16 -0400

    Fuse body tracking localization from 2 kinects

--- src/tf_broadcasters/src/tf_camera_body_all_joints_broadcaster.py
+++ src/tf_broadcasters/src/tf_camera_body_all_joints_broadcaster.py
@@ -30,6 +30,8 @@ import tf2_ros
 import geometry_msgs.msg
 import visualization_msgs.msg
 # import kinova_msgs.msg
+import numpy as np
+import math
 
 class Kinect2BodyAllJointsTf():
     def __init__(self):
@@ -38,6 +40,22 @@ class Kinect2BodyAllJointsTf():
         self.kinect_body_tracking_data_topic_name = rospy.get_param('~kinect_body_tracking_data_topic_name', "/body_tracking_data")
         self.parent_frame_id = rospy.get_param("~tf_camera_frame_id", "depth_camera_link")
         self.body_joints_tf_prefix = rospy.get_param("~body_joints_tf_prefix", "")
+
+        # Create covariance vector with size 36 = 6x6 for (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+        self.covariance_diagonal_max = rospy.get_param('~pose_covariance_diagonal_max',[1.,1.,1.,1.,1.,1.]) 
+        self.covariance_diagonal_min = rospy.get_param('~pose_covariance_diagonal_min',[1.,1.,1.,1.,1.,1.])
+        self.tf_broadcast_enable = rospy.get_param("~tf_broadcast_enable", True)
+
+        # Get reliability function parameters m and s
+        self.reliability_func_param_m = rospy.get_param('~reliability_func_param_m',0.0) 
+        self.reliability_func_param_s = rospy.get_param('~reliability_func_param_s',0.42)
+
+        # create body joint pose publishers
+        self.pubs_PoseWithCovarianceStamped = {}
+        for joint_name, joint_num in KINECT_JOINT_DICT.items():
+            topic_name = 'Pose_'+ self.body_joints_tf_prefix + joint_name.lower()
+            publisher = rospy.Publisher(topic_name, geometry_msgs.msg.PoseWithCovarianceStamped, queue_size=2)
+            self.pubs_PoseWithCovarianceStamped[joint_name] =  publisher
 
         self.tf_broadcaster = tf2_ros.TransformBroadcaster() # Create a tf broadcaster
         rospy.Subscriber(self.kinect_body_tracking_data_topic_name, visualization_msgs.msg.MarkerArray, self.handle_joint_pose)
@@ -58,9 +76,79 @@ class Kinect2BodyAllJointsTf():
                 t.transform.translation = joint_marker.pose.position
                 t.transform.rotation = joint_marker.pose.orientation
 
-                self.tf_broadcaster.sendTransform(t)
+                if self.tf_broadcast_enable:
+                    self.tf_broadcaster.sendTransform(t)
+
+                ## Publish pose with covariance of that body joint
+                # calculate its distance to camera
+                distance = self.calculate_distance(joint_marker.pose.position)
+                # calculate reliability score corresponding to that distance
+                score = self.calculate_reliability(distance)
+                # calculate corresponding covariance based on the reliability score
+                covariance = self.calculate_covariance(score)
+
+                # Create the PoseWithCovarianceStamped msg
+                pose_msg = geometry_msgs.msg.PoseWithCovarianceStamped()
+                pose_msg.header = t.header
+
+                pose_msg.pose.pose.position.x = t.transform.translation.x
+                pose_msg.pose.pose.position.y = t.transform.translation.y
+                pose_msg.pose.pose.position.z = t.transform.translation.z
+
+                pose_msg.pose.pose.orientation.x = t.transform.rotation.x
+                pose_msg.pose.pose.orientation.y = t.transform.rotation.y
+                pose_msg.pose.pose.orientation.z = t.transform.rotation.z
+                pose_msg.pose.pose.orientation.w = t.transform.rotation.w
+
+                pose_msg.pose.covariance = covariance
+
+                self.pubs_PoseWithCovarianceStamped[joint_name].publish(pose_msg)
+
+
+
         else:
             rospy.logerr_once("No body could be detected, waiting to detect..")
+
+    def calculate_distance(self,position):
+        return np.linalg.norm(np.array([position.x, position.y, position.z])) 
+
+    def calculate_reliability(self,distance):
+        # returns score between 0 - 1. See https://www.desmos.com/calculator/dprpp1kcu0 for details.
+        # 1: max reliable, 0: min reliable.
+
+        x = distance
+        m = self.reliability_func_param_m
+        s = self.reliability_func_param_s
+
+        log_normal = (1.0/(x*s*math.sqrt(2*math.pi))) * math.exp(-((math.log(x)-m)**2) / (2 * s**2) )
+
+        k = (1.0/(s*math.sqrt(2*math.pi))) * math.exp( (s**2 - 2*m) / 2.0 )# normalization factor 
+
+        # p = math.exp( m - s**2 ) # most reliable distance for debug purposes
+
+        score = log_normal / k
+        return score
+
+    def calculate_covariance(self,reliability_score):
+        covariance =  np.zeros(36)
+
+        self.covariance_diagonal_max
+        self.covariance_diagonal_min
+        
+        covariance[0]  = self.interpolate_linear(reliability_score,self.covariance_diagonal_min[0],self.covariance_diagonal_max[0]) # x
+        covariance[7]  = self.interpolate_linear(reliability_score,self.covariance_diagonal_min[1],self.covariance_diagonal_max[1]) # y
+        covariance[14] = self.interpolate_linear(reliability_score,self.covariance_diagonal_min[2],self.covariance_diagonal_max[2]) # z
+        covariance[21] = self.interpolate_linear(reliability_score,self.covariance_diagonal_min[3],self.covariance_diagonal_max[3]) # rot x
+        covariance[28] = self.interpolate_linear(reliability_score,self.covariance_diagonal_min[4],self.covariance_diagonal_max[4]) # rot y
+        covariance[35] = self.interpolate_linear(reliability_score,self.covariance_diagonal_min[5],self.covariance_diagonal_max[5]) # rot z
+        covariance = list(covariance) # convert to list of 36 floats
+
+        return covariance
+
+    def interpolate_linear(self,score,min,max):
+        return min + (1.0-score)*(max-min)
+        
+    
 
 
 KINECT_JOINT_DICT = {
--- test_launcher_all_oarbots.bash
+++ test_launcher_all_oarbots.bash
@@ -6,14 +6,12 @@ gnome-terminal --tab --title="TABLET" --command "bash -c \"source ~/.bashrc; ros
 gnome-terminal --tab --title="Oarbot_BLUE" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch oarbot_blue.launch; exec bash\"";
 gnome-terminal --tab --title="Oarbot_SILVER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch oarbot_silver.launch; exec bash\"";
 gnome-terminal --tab --title="NUC" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch nuc_kinect_with_aruco.launch; exec bash\"";
+gnome-terminal --tab --title="MASTER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch master_human_localization_ekf.launch; exec bash\"";
 sleep 6s;
 
 gnome-terminal --tab --title="RVIZ" --command "bash -c \"source ~/.bashrc; rosrun rviz rviz -d ~/catkin_ws_assistive/src/assistive_launch/config/rviz/all_oarbots_localized_with_aruco.rviz; exec bash\"";
 sleep 3s;
 
 # gnome-terminal --tab --title="ADMITTANCE" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch admittance_controller_collaborative.launch; exec bash\"";
-
-# gnome-terminal --tab --title="JOINT_FOLLOWER_BLUE" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch  oarbot_blue_body_single_joint_follower.launch; exec bash\"";
-# gnome-terminal --tab --title="JOINT_FOLLOWER_SILVER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch  oarbot_silver_body_single_joint_follower.launch; exec bash\"";
 
 
