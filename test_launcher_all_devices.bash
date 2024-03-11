e8d6d422c4d73cf29b8b6402a1a7055326e23b86
Burak Aksoy <burakaksoy20@gmail.com>
2024-03-11 09:48:24 -0400

    debug

--- test_launcher_all_devices.bash
+++ test_launcher_all_devices.bash
@@ -2,10 +2,10 @@
 sleep 1s;
 # gnome-terminal --tab --title="ROSCORE" --command "bash -c \"source ~/.bashrc; roscore; exec bash\"";
 # sleep 4s;
+gnome-terminal --tab --title="NUC" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch nuc_kinect_with_aruco.launch; exec bash\"";
 gnome-terminal --tab --title="TABLET" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch ui_tablet.launch; exec bash\"";
 gnome-terminal --tab --title="Oarbot_BLUE" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch oarbot_blue.launch; exec bash\"";
 gnome-terminal --tab --title="Oarbot_SILVER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch oarbot_silver.launch; exec bash\"";
-gnome-terminal --tab --title="NUC" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch nuc_kinect_with_aruco.launch; exec bash\"";
 gnome-terminal --tab --title="MASTER" --command "bash -c \"source ~/.bashrc; roslaunch assistive_launch master_human_localization_ekf.launch; exec bash\"";
 sleep 6s;
 
