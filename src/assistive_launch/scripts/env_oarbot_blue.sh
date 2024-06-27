a2197be3f4edca1f485656aa764b7743fd054e5d
Burak Aksoy <burakaksoy20@gmail.com>
2024-06-27 14:27:31 -0400

    Resolve kinect high CPU usage issue

--- src/assistive_launch/scripts/env_oarbot_blue.sh
+++ src/assistive_launch/scripts/env_oarbot_blue.sh
@@ -8,6 +8,7 @@ export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_MASTER_URI=http://192.168.1.100:11311/
 export ROS_IP=192.168.1.102
+export OMP_WAIT_POLICY=Passive # Needed to reduce CPU usage of Azure Kinect (see: https://github.com/microsoft/Azure-Kinect-Sensor-SDK/issues/1007)
 export DISPLAY=:0 # For Kinect remote launch
 
 exec "$@"
